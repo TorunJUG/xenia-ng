136b6aa6d394c110c1a28746bccf05858e5e2546
wololock <szymon.stepniak@gmail.com>
2014-09-20 22:28:45 +0200

    routing added

--- app/js/app.js
+++ app/js/app.js
@@ -2,4 +2,16 @@
 
 /* App Module */
 
-var xeniaApp = angular.module('xeniaApp', ['xeniaControllers', 'xeniaServices']);
+var xeniaApp = angular.module('xeniaApp', ['ngRoute', 'xeniaControllers', 'xeniaServices']);
+
+xeniaApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/events', {
+                templateUrl: 'partials/events.html',
+                controller: 'EventsListCtrl'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+    }]);
